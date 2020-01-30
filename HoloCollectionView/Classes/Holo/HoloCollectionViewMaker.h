b5ceda3e07fd623037d17dc32c23d450ddafd98f
gonghonglou <gonghonglou@icloud.com>
2020-01-30 23:54:07 +0800

    rename

---
+++ HoloCollectionView/Classes/Holo/HoloCollectionViewMaker.h
@@ -0,0 +1,16 @@
+//
+//  HoloCollectionViewMaker.h
+//  HoloCollectionView
+//
+//  Created by 与佳期 on 2020/1/30.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HoloCollectionViewMaker : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ HoloCollectionView/Classes/Holo/HoloCollectionViewMaker.m
@@ -0,0 +1,12 @@
+//
+//  HoloCollectionViewMaker.m
+//  HoloCollectionView
+//
+//  Created by 与佳期 on 2020/1/30.
+//
+
+#import "HoloCollectionViewMaker.h"
+
+@implementation HoloCollectionViewMaker
+
+@end
