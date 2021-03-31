c446deb0ca44ca9e01b91d5125a5ed0baf268a4b
gonghonglou <gonghonglou@icloud.com>
2021-03-31 21:55:09 +0800

    update HoloCollectionView.h

--- HoloCollectionView/Classes/HoloCollectionView.h
+++ HoloCollectionView/Classes/HoloCollectionView.h
@@ -8,37 +8,42 @@
 #ifndef HoloCollectionView_h
 #define HoloCollectionView_h
 
-// Holo
-#import <HoloCollectionView/HoloCollectionViewMacro.h>
-#import <HoloCollectionView/HoloCollectionViewProtocol.h>
-#import <HoloCollectionView/HoloCollectionViewCellProtocol.h>
-#import <HoloCollectionView/HoloCollectionViewHeaderProtocol.h>
-#import <HoloCollectionView/HoloCollectionViewFooterProtocol.h>
-#import <HoloCollectionView/HoloCollectionViewHeaderFooterProtocol.h>
+// Maker
 #import <HoloCollectionView/UICollectionView+HoloCollectionView.h>
 
-// HoloMaker (Item)
-#import <HoloCollectionView/HoloCollectionItem.h>
+// Maker (Item)
 #import <HoloCollectionView/HoloCollectionItemMaker.h>
 #import <HoloCollectionView/HoloCollectionViewItemMaker.h>
 
-// HoloMaker (Section)
-#import <HoloCollectionView/HoloCollectionSection.h>
+// Maker (Section)
 #import <HoloCollectionView/HoloCollectionSectionMaker.h>
 #import <HoloCollectionView/HoloCollectionViewSectionMaker.h>
 
-// HoloMaker (CollectionView)
+// Maker (CollectionView)
 #import <HoloCollectionView/HoloCollectionViewMaker.h>
 
-// HoloMaker (Update)
+// Maker (Update)
 #import <HoloCollectionView/HoloCollectionViewUpdateItemMaker.h>
 
-// HoloProxy
+// Core
+#import <HoloCollectionView/HoloCollectionViewProtocol.h>
 #import <HoloCollectionView/HoloCollectionViewProxy.h>
 #import <HoloCollectionView/HoloCollectionViewProxyData.h>
 #import <HoloCollectionView/UICollectionView+HoloCollectionViewProxy.h>
 
+// Core (Object)
+#import <HoloCollectionView/HoloCollectionItem.h>
+#import <HoloCollectionView/HoloCollectionSection.h>
 
+// Core (Protocol)
+#import <HoloCollectionView/HoloCollectionViewCellProtocol.h>
+#import <HoloCollectionView/HoloCollectionViewHeaderProtocol.h>
+#import <HoloCollectionView/HoloCollectionViewFooterProtocol.h>
+
+// Macro
+#import <HoloCollectionView/HoloCollectionViewMacro.h>
+
+// Deprecated
 #import <HoloCollectionView/UICollectionView+HoloDeprecated.h>
 #import <HoloCollectionView/HoloCollectionViewRowMaker.h>
 #import <HoloCollectionView/HoloCollectionViewUpdateRowMaker.h>
